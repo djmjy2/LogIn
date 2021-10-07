80a3921684d3588ba023264e24266fe602133e49
Hwi-Ram <djmjy2@naver.com>
2021-10-07 21:20:38 +0900

    login 작성 및 오탈자 수정

--- src/page/login.js
+++ src/page/login.js
@@ -1,6 +1,6 @@
 import template from './login.temlplate';
 import axios from 'axios';
-import textFieldTemplate from '../views/text-field.template';
+import textField from '../views/text-field.template';
 
 export default class Login {
   #template = template;
@@ -28,11 +28,44 @@ export default class Login {
     this.#fields.push(passwordField);  
   }
 
-  #onSubmit = e => {
+  #onSubmit = e => { //아직 이해가 잘안된다. 좀더 공부해봐야겠다
+    e.preventDefault();
 
+    const loginData = this.#fields
+      .map(field => ({ [field.name]: field.value }))
+      .reduce((a, b) => ({...a, ...b}), {});
+
+    axios.post('/api/authentication', loginData)
+      .then(result => {
+        return result.data.result;
+      })
+      .then(({ id, token }) => {
+        const options = { headers: {token} };
+        this.#data.store.token = token;
+
+        return axios.all([
+          axios.get(`/api/user/${id}`, options),
+          axios.get(`/api/user/${id}/posts`, options),
+        ]);
+      })
+      .then(([profile, posts]) => {
+        this.#data.store.userProfile = profile.data.result;
+        this.#data.store.userPosts = posts.data.result;
+
+        location.href = '/#/profile';
+      })
+      .catch(error => {
+        this.#loginFail = true;
+        this.render();
+      });
   }
 
   render = () => {
-    
+    this.#container.innerHTML = this.#template({ ...this.#data, loginFail: this.#loginFail });
+    this.#fields.forEach(field => {
+      field.render(ture);
+    });
+
+    this.#container.addEventListener('submit', this.#onSubmit);
   }
 }
