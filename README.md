9d620835cfd39be19a9ed5fc18be7b3684a4b3ed
Hwi-Ram <djmjy2@naver.com>
2021-10-11 23:00:26 +0900

    readme updata
    텍스트필드 js 일부 작성 및 readme 업데이트.
    
    axios 학습

--- README.md
+++ README.md
@@ -3,5 +3,12 @@
 - randomuser api를 활용하여 가짜 프로필정보를 받아와 클라이언트로 내려준다
 - jsonplaceholder api를 활용하여 임의의 데이터를 받아와 프로필의 post로 활용
 
+
+ref
+- https://axios-http.com/docs/intro
+- https://randomuser.me
+- https://jsonplaceholder.typicode.com/
+
+
 -2021.10.08
  javescript 는 끝이없다.
--- src/views/text-field.js
+++ src/views/text-field.js
@@ -1,1 +1,13 @@
 import { nextTick } from '../utils';
+import { RequireRule } from '../constant';
+import template from './text-field.template';
+import CoreField from './core';
+
+export default class TextField extends CoreField {
+  #updated = false;
+
+  constructor(container, data) {
+    
+  }
+
+}
