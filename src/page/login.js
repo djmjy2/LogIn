a5e5db31bb5fa791312bd60ac7cd438f06ac2d4d
Hwi-Ram <djmjy2@naver.com>
2021-10-17 23:21:54 +0900

    오탈자 수정 및 불필요한 주석 제거

--- src/page/login.js
+++ src/page/login.js
@@ -1,6 +1,6 @@
 import template from './login.temlplate';
 import axios from 'axios';
-import textField from '../views/text-field.template';
+import TextField from '../views/text-field';
 
 export default class Login {
   #template = template;
@@ -17,7 +17,7 @@ export default class Login {
   }
 
   #initialize = () => {
-    const idField = new textField('#login-fields', {
+    const idField = new TextField('#login-fields', {
       id: 'userid', label: '아이디', type: 'text', placeholder: '아이디를 입력하세요', require: true,
     });
     const passwordField = new TextField('#logid-fields', {
@@ -28,33 +28,35 @@ export default class Login {
     this.#fields.push(passwordField);  
   }
 
-  #onSubmit = e => { //아직 이해가 잘안된다. 좀더 공부해봐야겠다
+  #onSubmit = e => {
     e.preventDefault();
 
     const loginData = this.#fields
       .map(field => ({ [field.name]: field.value }))
-      .reduce((a, b) => ({...a, ...b}), {});
+      .reduce((a, b) => ({ ...a, ...b }), {});
 
     axios.post('/api/authentication', loginData)
       .then(result => {
         return result.data.result;
       })
       .then(({ id, token }) => {
-        const options = { headers: {token} };
+        const options = { headers: { token } };
         this.#data.store.token = token;
 
-        return axios.all([
+        axios.all([
           axios.get(`/api/user/${id}`, options),
           axios.get(`/api/user/${id}/posts`, options),
-        ]);
+        ]).then(([profile, posts]) => {
+          this.#data.store.userProfile = profile.data.result;
+          this.#data.store.userPosts = posts.data.results;
+  
+          location.href = '/#/profile';
+        }).catch(error => {      
+          this.#loginFail = true;
+          this.render();
+        });
       })
-      .then(([profile, posts]) => {
-        this.#data.store.userProfile = profile.data.result;
-        this.#data.store.userPosts = posts.data.result;
-
-        location.href = '/#/profile';
-      })
-      .catch(error => {
+      .catch(error => {      
         this.#loginFail = true;
         this.render();
       });
@@ -63,7 +65,7 @@ export default class Login {
   render = () => {
     this.#container.innerHTML = this.#template({ ...this.#data, loginFail: this.#loginFail });
     this.#fields.forEach(field => {
-      field.render(ture);
+      field.render(true);
     });
 
     this.#container.addEventListener('submit', this.#onSubmit);
